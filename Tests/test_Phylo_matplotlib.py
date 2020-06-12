0512ccd4d057b52a4832a5349b08d8a4129ce406
Jagoda Kuśnierz <jagoda.kusnierz@gmail.com>
2020-06-12 15:22:41 +0200

    Update test_Phylo_matplotlib.py
    style changes

--- Tests/test_Phylo_matplotlib.py
+++ Tests/test_Phylo_matplotlib.py
@@ -46,21 +46,21 @@ except ImportError:
     raise MissingExternalDependencyError(
         "Install numpy if you want to use Bio.Phylo._utils."
     ) from None
-    
+
 try:
     import random
 except ImportError:
     raise MissingExternalDependencyError(
         "Install random if you want to use Bio.Phylo._utils."
     ) from None
-    
+
 try:
     import os
 except ImportError:
     raise MissingExternalDependencyError(
         "Install os if you want to use Bio.Phylo._utils."
     ) from None
-    
+
 try:
     import shutil
 except ImportError:
@@ -74,14 +74,14 @@ except ImportError:
     raise MissingExternalDependencyError(
         "Install imageio if you want to use Bio.Phylo._utils."
     ) from None
-    
+
 try:
     from pygifsicle import optimize
 except ImportError:
     raise MissingExternalDependencyError(
         "Install pygifsicle if you want to use Bio.Phylo._utils."
     ) from None
-    
+
 # Example PhyloXML file
 EX_DOLLO = "PhyloXML/o_tol_332_d_dollo.xml"
 EX_APAF = "PhyloXML/apaf.xml"
@@ -101,18 +101,19 @@ class UtilTests(unittest.TestCase):
         # Fancier options
         Phylo.draw(apaf, do_show=False, branch_labels={apaf.root: "Root"})
         Phylo.draw(
-            apaf, do_show=False, branch_labels=lambda c: c.branch_length  # noqa: E731
+            apaf, do_show=False,
+            branch_labels=lambda c: c.branch_length  # noqa: E731
         )
         # Check that graph saves correctly
-        wykres.draw(apaf, do_show=False, save_name="test")
+        Phylo.draw(apaf, do_show=False, save_name="test")
         assert os.path.exists("test.png")
         if not os.stat("test.png").st_size > 1:
-            raise FileNotFoundError("Size of the saved graph is too small.") from None
+            raise FileNotFoundError(
+                "Size of the saved graph is too small.") from None
         os.remove("test.png")
 
     def test_draw_with_label_colors_dict(self):
         """Layout tree with label colors as dict.
-
         Run the tree layout algorithm with a label_colors argument passed in
         as a dictionary. Don't display tree.
         """
@@ -132,7 +133,6 @@ class UtilTests(unittest.TestCase):
 
     def test_draw_with_label_colors_callable(self):
         """Layout tree with label colors as callable.
-
         Run the tree layout algorithm with a label_colors argument passed in
         as a callable. Don't display tree.
         """
@@ -140,8 +140,8 @@ class UtilTests(unittest.TestCase):
         dollo = Phylo.read(EX_DOLLO, "phyloxml")
         apaf = Phylo.read(EX_APAF, "phyloxml")
 
-        label_colors_dollo = lambda label: "r" if label == "f_50" else "k"  # noqa: E731
-        label_colors_apaf = lambda label: "r"  # noqa: E731
+        def label_colors_dollo(label): return "r" if label == "f_50" else "k"
+        def label_colors_apaf(label): return "r"  # noqa: E731
 
         Phylo.draw(dollo, label_colors=label_colors_dollo, do_show=False)
         Phylo.draw(apaf, label_colors=label_colors_apaf, do_show=False)
@@ -153,7 +153,7 @@ class UtilTests(unittest.TestCase):
         Phylo.draw_ascii(tree, file=handle)
         Phylo.draw_ascii(tree, file=handle, column_width=120)
         handle.close()
-    
+
     def test_draw_with_photos(self):
         """Layout tree with added photos"""
         pyplot.ioff()  # Turn off interactive display
@@ -162,44 +162,45 @@ class UtilTests(unittest.TestCase):
         os.chdir(current+"/PhyloXML/photos")
         Phylo.draw(photo, do_show=False, photos=True)
         os.chdir(current)
-    
+
     def test_width(self):
         """Layout tree with added branch widths"""
         pyplot.ioff()  # Turn off interactive display
         photo = Phylo.read(EX_PHOTO, "phyloxml")
-        
+
         Phylo.draw(photo, do_show=False, width=True)
-        
+
     def test_multiple(self):
         """Multiple graphs on one plot"""
         pyplot.ioff()  # Turn off interactive display
         photo = Phylo.read(EX_PHOTO, "phyloxml")
         apaf = Phylo.read(EX_APAF, "phyloxml")
-        test_list=[(photo, 60), (apaf, 40)]
-        
+        test_list = [(photo, 60), (apaf, 40)]
+
         Phylo.draw(test_list, do_show=False)
-        
+
     def test_visualize_changes(self):
         """Generate gif from multiple trees"""
         pyplot.ioff()  # Turn off interactive display
         photo = Phylo.read(EX_PHOTO, "phyloxml")
         apaf = Phylo.read(EX_APAF, "phyloxml")
         test_list = [photo, apaf]
-        
+
         Phylo.visualize_changes(test_list, "test_gif1")
         Phylo.visualize_changes(test_list, "test_gif2", optimized=True)
-        
+
         # Check that gifs save correctly
         assert os.path.exists("test_gif1.gif")
         assert os.path.exists("test_gif2.gif")
         if not os.stat("test_gif1.gif").st_size > 1:
-            raise FileNotFoundError("Size of the saved gif is too small.") from None
+            raise FileNotFoundError(
+                "Size of the saved gif is too small.") from None
         if not os.stat("test_gif2.gif").st_size > 1:
-            raise FileNotFoundError("Size of the saved optimized gif is too small.") from None
+            raise FileNotFoundError(
+                "Size of the saved optimized gif is too small.") from None
 
         os.remove("test_gif1.gif")
         os.remove("test_gif2.gif")
-            
 
 
 if __name__ == "__main__":
